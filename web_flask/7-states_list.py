a2c5da963ac2f6a00c137da9185e8c954b9c8b06
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-21 22:42:23 +0000

    states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 ''' Start a Flask '''
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
 app = Flask(__name__, template_folder='./templates')
-app.url_map.strict_slashes = False
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -21,4 +19,4 @@ def remove_session(response_or_exc):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
