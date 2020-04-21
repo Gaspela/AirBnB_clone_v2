5e4bdd08669e57f25660df5d778b7d31844abda5
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-21 20:40:02 +0000

    Return python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+''' Start Flask. '''
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    ''' Return page. '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Return page. '''
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    ''' Return c page data. '''
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    ''' Return python page data. '''
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+if __name__ == '__main__':
+    app.run()
